9444d5776ef49b3283014bb3f2cb88be52b9c143
mustafaryad1 <mustafaryad1@gmail.com>
2021-07-13 14:35:54 +0200

    Finish Contacts list and Paginations

---
+++ frontend/efile/src/app/home/contact.model.ts
@@ -0,0 +1,8 @@
+export class Contact{
+  // tslint:disable-next-line:variable-name
+  _id: string | undefined;
+  name: string | undefined;
+  phone: string | undefined;
+  address: string | undefined;
+  notes: string | undefined;
+}
--- frontend/efile/src/app/home/home.component.ts
+++ frontend/efile/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactsService } from '../services/contacts.service';
+import { Contact } from './contact.model';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +8,17 @@ import { ContactsService } from '../services/contacts.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  contacts:Contact[] = [];
   constructor(private contactService:ContactsService) { }
-
+  page = 1;
   ngOnInit(): void {
-
     // for get all contacts
-    // this.contactService.get_contacts().subscribe(res=>{
-    //   console.log(res)
-    // })
+    this.contactService.get_contacts().subscribe(res=>{
+      if(this.page){
+      this.contacts = res.contacts.slice(5*(this.page-1),5*this.page);
+      }
+    })
+
     // for add contact
     // this.contactService.add_contact({name:"woka2",phone:"123",address:"sdf",notes:"wo"}).subscribe(res=>{
     //   console.log(res)
@@ -29,6 +32,24 @@ export class HomeComponent implements OnInit {
     //   console.log(res)
     // })
 
+  }
+  previous(){
+    if (this.page < 1){
+      this.page = 1
+    }else{
+
+      this.page -=1;
+    }
+    this.ngOnInit()
+  }
+  next(){
+    if(this.page*5> this.contacts.length){
+      this.page =this.page;
+    }else{
+      this.page +=1
+    }
+
+    this.ngOnInit()
   }
 
 
